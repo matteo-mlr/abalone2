68d779a505f7682ebe59a654e8a8aa8c4eb92965
daniel <1821027@stud.hs-mannheim.de>
2019-10-24 15:10:19 +0200

    event

--- AbaloneZwei/src/abaloneZwei/Event.java
+++ AbaloneZwei/src/abaloneZwei/Event.java
@@ -2,4 +2,95 @@ package abaloneZwei;
 
 public class Event {
 
+	private Profil ersteller;
+	private String titel;
+	private String beschreibung;
+	private String kategorie;
+	private int zeitraum;
+	private Profil[] teilnehmer;
+	private int teilnehmerAnzahl;
+	private Rezension[] rezensionen;
+	//private Standort standort;
+	//private bilder img[];
+	//private kategorieIcon img;
+	
+	public Event(Profil ersteller, String titel,String kategorie, int zeitraum,int teilnehmerAnzahl) {
+		
+		setErsteller(ersteller);
+		setTitel(titel);
+		setKategorie(kategorie);
+		setZeitraum(zeitraum);
+		setTeilnehmerAnzahl(teilnehmerAnzahl);
+		
+		setTeilnehmer(new Profil[teilnehmerAnzahl]);
+		
+	}
+
+	public Profil getErsteller() {
+		return ersteller;
+	}
+
+	private void setErsteller(Profil ersteller) {
+		this.ersteller = ersteller;
+	}
+
+	public String getTitel() {
+		return titel;
+	}
+
+	private void setTitel(String titel) {
+		this.titel = titel;
+	}
+
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+
+	private void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+
+	public String getKategorie() {
+		return kategorie;
+	}
+
+	private void setKategorie(String kategorie) {
+		this.kategorie = kategorie;
+	}
+
+	public int getZeitraum() {
+		return zeitraum;
+	}
+
+	private void setZeitraum(int zeitraum) {
+		this.zeitraum = zeitraum;
+	}
+
+	public Profil[] getTeilnehmer() {
+		return teilnehmer;
+	}
+
+	private void setTeilnehmer(Profil[] teilnehmer) {
+		this.teilnehmer = teilnehmer;
+	}
+
+	public int getTeilnehmerAnzahl() {
+		return teilnehmerAnzahl;
+	}
+
+	private void setTeilnehmerAnzahl(int teilnehmerAnzahl) {
+		this.teilnehmerAnzahl = teilnehmerAnzahl;
+	}
+
+	public Rezension[] getRezensionen() {
+		return rezensionen;
+	}
+
+	private void setRezensionen(Rezension[] rezensionen) {
+		this.rezensionen = rezensionen;
+	}
+	
+	
+	
+	
 }
