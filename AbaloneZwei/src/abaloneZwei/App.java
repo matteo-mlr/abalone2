2c2734a592016cd0861acd211004a9dcda0a2708
Matteo <1824001@stud.hs-mannheim.de>
2019-10-24 14:33:25 +0200

    Die ersten Zeilen.

--- AbaloneZwei/src/abaloneZwei/App.java
+++ AbaloneZwei/src/abaloneZwei/App.java
@@ -1,5 +1,32 @@
 package abaloneZwei;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+	
+	private static ArrayList<Profil> profile;
+	
+	public static void main (String[] args) {
+		
+		profile = new ArrayList<>();
+	
+		Scanner sc = new Scanner(System.in);
+		
+		profilAnlegen(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine());
+		
+	}
+	
+	private static void profilAnlegen (String vorname, String nachname, String nutzername, String email, String passwort) {
+		
+		if (vorname == null && nachname == null && nutzername == null && email == null && passwort == null) {
+		
+			throw new RuntimeException("Fehler");
+		
+		}
+		
+		profile.add(new Profil(vorname, nachname, nutzername, email, passwort));
+		
+	}
 
 }
--- AbaloneZwei/src/abaloneZwei/Profil.java
+++ AbaloneZwei/src/abaloneZwei/Profil.java
@@ -1,5 +1,86 @@
 package abaloneZwei;
 
 public class Profil {
+	
+	private String vorname;
+	private String nachname;
+	private String nutzername;
+	private String email;
+	private String passwort;
+	
+	public Profil (String vorname, String nachname, String nutzername, String email, String passwort) {
+		
+		setVorname(vorname);
+		setNachname(nachname);
+		setNutzername(nutzername);
+		setEmail(email);
+		setPasswort(passwort);
+		
+	}
+	
+	private void setVorname (String vorname) {
+		
+		if (vorname == null) {
+			
+			// Fehlermanagement
+			return;
+			
+		}
+		
+		this.vorname = vorname;
+		
+	}
+	
+	private void setNachname (String nachname) {
+		
+		if (nachname == null) {
+			
+			// Fehlermanagement
+			return;
+			
+		}
+		
+		this.nachname = nachname;
+		
+	}
+	
+	private void setNutzername (String nutzername) {
+		
+		if (nutzername == null) {
+			
+			// Fehlermanagement
+			return;
+			
+		}
+		
+		this.nutzername = nutzername;
+		
+	}
+	
+	private void setEmail (String email) {
+		
+		if (email == null) {
+			
+			// Fehlermanagement
+			return;
+			
+		}
+		
+		this.email = email;
+		
+	}
+
+	private void setPasswort (String passwort) {
+	
+	if (passwort == null) {
+		
+		// Fehlermanagement
+		return;
+		
+	}
+	
+	this.passwort = passwort;
+	
+	}
 
 }
